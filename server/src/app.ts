1a1dcd6d53cf3e091dd3896bcdd926f48b23eee5
Grishin Anton <grishin_rostov@mail.ru>
2022-06-08 21:52:38 +0300

    Правка порта

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ export class App {
 		@inject(TYPES.StatisticController) private statisticController: StatisticController,
 	) {
 		this.app = express();
-		this.port = 3000;
+		this.port = Number(process.env.PORT) || 8000;
 	}
 
 	useMiddleware(): void {

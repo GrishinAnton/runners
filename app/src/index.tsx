f603b0113148d8ce3497c5d03a492f126a0828cc
Grishin Anton <grishin_rostov@mail.ru>
2022-06-09 09:03:43 +0300

    Правки

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import { API_SERVER_URL } from './config';
+
+axios.defaults.baseURL = API_SERVER_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
